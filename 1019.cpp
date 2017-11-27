ffaca058d9408f8d533aaf6cead6e98870cf6a79
Jeff Ng <nuaajeff@outlook.com>
2017-11-27 17:16:31 +0800

    PAT-A-1019

---
+++ 1019.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int> change(long long num, long long base)
+{
+    /**
+    string a = "";
+    if (num == 0)
+        return "0";
+    while (num != 0)
+    {
+        char b = num % base + '0';
+        a = b + a;
+        num /= base;
+    }
+    return a;
+    **/
+    if (num == 0) return vector<int>{0};
+    vector<int> v;
+    while(num)
+    {
+        v.push_back(num % base);
+        num /= base;
+    }
+    return v;
+}
+
+int main()
+{
+    long long num, base;
+    cin >> num >> base;
+    vector<int> num_b = change(num, base);
+    vector<int> r_num_b = num_b;
+    reverse(r_num_b.begin(), r_num_b.end());
+    num_b == r_num_b ? cout << "Yes" << endl : cout << "No" << endl;
+    for (int i = 0; i < r_num_b.size(); i++)
+    {
+        cout << r_num_b[i];
+        if (i != r_num_b.size() - 1)
+            cout << " ";
+    }
+}
